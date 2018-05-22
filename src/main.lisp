ab075a45757857d268f75f9a2ed2df343a65cbb8
f_egoavil@hotmail.com <defunkydrummer@users.noreply.github.com>
2018-05-22 16:35:42 -0500

    temporary bug fix for XSLX files with no inner number format information

--- src/main.lisp
+++ src/main.lisp
@@ -1,7 +1,7 @@
 (in-package :lisp-xl)
 
 ;; --- NOTE: to be reviewed...
-(declaim (optimize (speed 3) (debug 0) (safety 0) (space 0)))
+(declaim (optimize (speed 1) (debug 3) (safety 3) (space 0)))
 
 
 (defparameter *element-type* '(unsigned-byte 8))
@@ -103,7 +103,8 @@
   
   "Generalized Process sheet (as struct)"
   (declare (type sheet sheet-struct)
-           (type fixnum initial-row max-row)
+           (type fixnum initial-row)
+           (type (or null fixnum) max-row)
            (type (function (fixnum fixnum (or string null) (or string null)(or string null)) (or cons null)) 
                   column-process-function)
            (type function row-begin-function
@@ -206,7 +207,8 @@
                  If row-function is defined, then <the row values will not be returned by process-sheet.
 "
   (declare (type sheet sheet-struct)
-           (type fixnum initial-row max-row)
+           (type fixnum initial-row)
+           (type (or fixnum null) max-row)
            (type boolean silent debug-print)
            (type (or cons null) column-list)
            (type (or (function (cons)) null) row-function))
@@ -288,7 +290,8 @@
                                                    (column-list nil)) ;; get only selected columns (list of indexes)
   "Inspect how cell type changes from row to row. Returns number of changes."
   (declare (type sheet sheet-struct)
-           (type fixnum initial-row max-row)
+           (type fixnum initial-row)
+           (type (or null fixnum) max-row)
            (type (or cons null) column-list))
   (let* ((number-formats (sheet-number-formats sheet-struct))
          (info nil)
